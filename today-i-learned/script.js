f3f3c1aa51c8e040af61f6a9ddc1650a2757fd2a
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2023-02-23 20:50:09 +0800

    Objects

--- today-i-learned/script.js
+++ today-i-learned/script.js
@@ -102,3 +102,22 @@ console.log(text);
 
 const newFact = [...fact, "society"];
 console.log(newFact);
+
+const factObj = {
+  text: "Lisbon is the capital of Portugal",
+  category: "society",
+  createdIn: 2015,
+  isCorrect: true,
+  createSummary: function () {
+    return `The fact "${
+      this.text
+    }" is from category ${this.category.toUpperCase()}`;
+  },
+};
+
+console.log(factObj.text);
+console.log(factObj["text"]);
+
+const { category, isCorrect } = factObj;
+console.log(category);
+console.log(factObj.createSummary());
