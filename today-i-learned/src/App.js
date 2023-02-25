19f9135c1c4ff16d697d9b9c31319ee365935b81
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2023-02-25 23:22:31 +0800

    Meeting JSX and Creating the App Component

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -1,5 +1,16 @@
+import "./style.css";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
 }
 
 export default App;
