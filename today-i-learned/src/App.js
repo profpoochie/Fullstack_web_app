a12328ca42344c94996c537d2901d628495e34cd
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2023-02-27 17:22:59 +0800

    Creating the Header Component

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -48,32 +48,11 @@ function Counter() {
 }
 
 function App() {
-  // 1. Define state variable
   const [showForm, setShowForm] = useState(false);
-  const appTitle = "Today I Learned";
 
   return (
     <>
-      {/* HEADER */}
-      <header className="header">
-        <div className="logo">
-          <img
-            src="logo.png"
-            height="68"
-            width="68"
-            alt="Today I learned Logo"
-          />
-          <h1>{appTitle}</h1>
-        </div>
-        <button
-          className="btn btn-large btn-open"
-          // 3. Update state variable
-          onClick={() => setShowForm((show) => !show)}
-        >
-          Share a fact
-        </button>
-      </header>
-      {/* Use state variable */}
+      <Header showForm={showForm} setShowForm={setShowForm} />
       {showForm ? <NewFactForm /> : null}
 
       <main className="main">
@@ -83,6 +62,25 @@ function App() {
     </>
   );
 }
+
+function Header({ showForm, setShowForm }) {
+  const appTitle = "Today I Learned";
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I learned Logo" />
+        <h1>{appTitle}</h1>
+      </div>
+      <button
+        className="btn btn-large btn-open"
+        onClick={() => setShowForm((show) => !show)}
+      >
+        {showForm ? "Close" : "Share a fact"}
+      </button>
+    </header>
+  );
+}
+
 function NewFactForm() {
   return <form className="fact-form">Fact form</form>;
 }
