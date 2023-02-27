3c24520d79a32f6132e5499e75bd676fd190a7f6
Marvin Borhillo <40102065+profpoochie@users.noreply.github.com>
2023-02-27 23:03:48 +0800

    Adding a New Fact – Part 2

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -49,15 +49,18 @@ function Counter() {
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [facts, setFacts] = useState(initialFacts);
 
   return (
     <>
       <Header showForm={showForm} setShowForm={setShowForm} />
-      {showForm ? <NewFactForm /> : null}
+      {showForm ? (
+        <NewFactForm setFacts={setFacts} setShowForm={setShowForm} />
+      ) : null}
 
       <main className="main">
         <CategoryFilter />
-        <FactList />
+        <FactList facts={facts} />
       </main>
     </>
   );
@@ -91,7 +94,7 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
-function NewFactForm() {
+function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
   const [source, setSource] = useState("http://example.com");
   const [category, setCategory] = useState("");
@@ -123,12 +126,17 @@ function NewFactForm() {
         votesInteresting: 0,
         votesMindblowing: 0,
         votesFalse: 0,
-        createdIn: new Date().getCurrentYear(),
+        createdIn: new Date().getFullYear(),
       };
 
       // 4. Add the new fact to the UI; add the fact to the state
+      setFacts((facts) => [newFact, ...facts]);
       // 5. Reset input fields
+      setText("");
+      setSource("");
+      setCategory("");
       // 6. Close the form
+      setShowForm(false);
     }
   }
 
@@ -182,9 +190,9 @@ function CategoryFilter() {
   );
 }
 
-function FactList() {
+function FactList({ facts }) {
   // Temporary
-  const facts = initialFacts;
+
   return (
     <section>
       <ul className="fact-list">
